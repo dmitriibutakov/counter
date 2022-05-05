0776ceb275b6a45baa869fd3e40f000c7091beaf
dmitryload <sportyounde@gmail.com>
2022-05-05 10:44:15 +0300

    debug strict mode

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,26 @@ import CounterSet from "./components/CounterSet/CounterSet";
 import CounterInc from "./components/CounterInc/CounterInc";
 
 export type ErrorType = 'set' | 'inc'
+
 function App() {
     const [minLimit, setMinLimit] = useState<number>(0)
     const [maxLimit, setMaxLimit] = useState<number>(5)
-    const [count, setCount] = useState<number>(minLimit)
+    const [count, setCount] = useState<number>(0)
     const [error, setError] = useState<ErrorType>('set')
 
-  useEffect(() => {
-    let minLimitValue = localStorage.getItem('minLimit')
-    if (minLimitValue) {
-        setCount(JSON.parse(minLimitValue))
-        setMinLimit(JSON.parse(minLimitValue))
-    }
-    let maxLimitValue = localStorage.getItem('maxLimit')
-      maxLimitValue && setMaxLimit(JSON.parse(maxLimitValue))
-      let startError = localStorage.getItem('error')
-      startError && setError(JSON.parse(startError))
-}, [])
-
+    useEffect(() => {
+        let minLimitValue = localStorage.getItem('minLimit')
+        if (minLimitValue) {
+            setCount(+minLimitValue)
+            setMinLimit(+minLimitValue)
+        }
+        let maxLimitValue = localStorage.getItem('maxLimit')
+        maxLimitValue && setMaxLimit(JSON.parse(maxLimitValue))
+    }, [])
     useEffect(() => {
         localStorage.setItem('minLimit', JSON.stringify(minLimit))
         localStorage.setItem('maxLimit', JSON.stringify(maxLimit))
-        localStorage.setItem('error', JSON.stringify(error))
-    }, [minLimit,maxLimit, error])
+    }, [minLimit, maxLimit])
 
     const countUp = () => {
         setCount(count + 1)
@@ -34,7 +31,7 @@ function App() {
     const resetCount = () => {
         setCount(minLimit)
     }
-    const onChangeHandlerMax = (e:ChangeEvent<HTMLInputElement>) => {
+    const onChangeHandlerMax = (e: ChangeEvent<HTMLInputElement>) => {
         setMaxLimit(e.currentTarget.valueAsNumber)
         setError("inc")
     }
@@ -65,7 +62,7 @@ function App() {
                         countUp={countUp}
                         resetCount={resetCount}
                         classCounter={classCounter}
-                        />
+            />
         </div>
     );
 }
